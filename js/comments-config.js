a37c72091a07f851dcafd5303d00d9c9159d14f7
tinnguyen-hm <tin.nguyen@hikemedical.com>
2026-09-10 14:09:02 -0500

    Update comments-config.js

--- js/comments-config.js
+++ js/comments-config.js
@@ -1,3 +1,3 @@
 // Paste the Apps Script /exec URL here after deploying the comment backend for
 // this client (see README.md). Each client needs its own separate deployment.
-window.COMMENTS_ENDPOINT = "";
+window.COMMENTS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwvJgXelfx9gdi1jztk4QdjDsMm7qSJ25eIDDgEIrqz_l65zjzAoFVmsh64iF6CMnl7GA/exec";
